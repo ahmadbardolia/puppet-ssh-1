f5d473000b6cf7f28f2364c98500eb1e42866dea
Travis N. Thomse n <travis@linuxacademy.com>
2017-03-03 18:19:07 +0000

    add data function

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,11 +43,14 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class ssh(
-  Boolean $permit_root_login = $::ssh::params::permit_root_login,
-  Integer $port              = $::ssh::params::port,
-  String $package_name       = $::ssh::params::package_name,
-  String $service_name       = $::ssh::params::service_name,
-) inherits ::ssh::params {
+  String $package_name,
+  String $service_name,
+  String $service_ensure,
+  String  $ensure,
+  Boolean $service_enable,
+  Boolean $permit_root_login = false,
+  Integer $port              = 22,
+) {
   class { '::ssh::service': }
   class { '::ssh::config': }
   class { '::ssh::install': }
