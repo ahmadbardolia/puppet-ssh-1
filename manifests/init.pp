0f6521c7426ae2f5dee698a7d71a970f6cab9619
Travis N. Thomse n <travis@linuxacademy.com>
2017-03-03 15:32:34 +0000

    files and templates

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,8 +42,18 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class ssh {
-  class { 'ssh::install': } ->
-  class { 'ssh::service': }
-
+class ssh(
+  Boolean $permit_root_login = $::ssh::params::permit_root_login,
+  Integer $port              = $::ssh::params::port,
+  String $package_name       = $::ssh::params::package_name,
+  String $service_name       = $::ssh::params::service_name,
+) inherits ::ssh::params {
+  class { '::ssh::service': }
+  class { '::ssh::config': }
+  class { '::ssh::install': }
+  
+  Class['::ssh::install']
+  -> Class['::ssh::config']
+  ~> Class['::ssh::service']
+  -> Class['ssh']
 }
